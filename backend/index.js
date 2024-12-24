430bf9c396741b8df39c9e3f87cb6f1bc4e2e54f
Varun Yelluru <varunyelluru@Varuns-MacBook-Pro.local>
2024-12-23 22:43:03 -0800

    yay

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.post('/api', async (req, res) => {
+    try {
+        // Get file names from the req body
+        const { input_file, output_format_file, output_file } = req.body;
+
+        // Create payload to send to api gateway
+        const payload = {
+            input_file,
+            output_format_file,
+            output_file,
+        };
+
+        // Post request to url with payload
+        const response = await axios.post(AWS_API_GATEWAY_URL, payload);
+        console.log("Lambda Response:", response.data);
+
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        console.error("Error triggering Lambda:", error.message);
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Backend server is running on http://localhost:${PORT}`);
+});
+
+
+/*
+curl -X POST http://localhost:3000/api \
+-H "Content-Type: application/json" \
+-d '{
+    "input_file": "input_file.txt",
+    "output_format_file": "output_file_format1.txt",
+    "output_file": "output_file_converted.txt"
+    }'
+*/
