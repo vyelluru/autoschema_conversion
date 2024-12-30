b7ab17ba8a4a46ac6c8f807c5cbcb16a6249b869
Varun Yelluru <varunyelluru@Varuns-MacBook-Pro.local>
2024-12-29 22:12:51 -0800

    react routes v1

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>"Hi Home"</h1>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Mapping.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Mapping = () => {
+    return (
+        <div>
+            <h1>"Hi Mapping"</h1>
+        </div>
+    )
+}
